ba38096402c4c8f5417b92c6e8e47b2f7c14cec9
Edwin <1135365441@qq.com>
2023-02-07 07:22:07 +0800

    学成在线2.0

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
@@ -0,0 +1,36 @@
+package com.xuecheng.base.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 分页查询通用参数
+ * @date 2022/9/6 14:02
+ */
+@Data
+@ToString
+public class PageParams {
+
+    //当前页码默认值
+    public static final long DEFAULT_PAGE_CURRENT = 1L;
+    //每页记录数默认值
+    public static final long DEFAULT_PAGE_SIZE = 10L;
+
+    //当前页码
+    private Long pageNo = DEFAULT_PAGE_CURRENT;
+
+    //每页记录数默认值
+    private Long pageSize = DEFAULT_PAGE_SIZE;
+
+    public PageParams() {
+
+    }
+
+    public PageParams(long pageNo, long pageSize) {
+        this.pageNo = pageNo;
+        this.pageSize = pageSize;
+    }
+    
+}
---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/model/dto/QueryCourseParamsDto.java
@@ -0,0 +1,23 @@
+package com.xuecheng.model.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 课程查询参数Dto
+ * @date 2022/9/6 14:36
+ */
+@Data
+@ToString
+public class QueryCourseParamsDto {
+
+    //审核状态
+    private String auditStatus;
+    //课程名称
+    private String courseName;
+    //发布状态
+    private String publishStatus;
+
+}
