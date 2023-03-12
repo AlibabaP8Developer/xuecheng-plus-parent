735cd951d50cc78153e79d4a3e8820a2690e3bfb
Edwin <1135365441@qq.com>
2023-03-12 15:33:10 +0800

    学成在线2.0

---
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/GatewayApiApplication.java
@@ -0,0 +1,11 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApiApplication.class, args);
+    }
+}
