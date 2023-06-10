7b8a97b43562ddadb2575b406411339ade0ff9cf
Edwin <1135365441@qq.com>
2023-06-10 09:42:38 +0800

    学成在线2.0

---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileService.java
@@ -0,0 +1,26 @@
+package com.xuecheng.media.service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.media.model.dto.QueryMediaParamsDto;
+import com.xuecheng.media.model.po.MediaFiles;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 媒资文件管理业务类
+ * @date 2022/9/10 8:55
+ */
+public interface MediaFileService {
+
+    /**
+     * @param pageParams          分页参数
+     * @param queryMediaParamsDto 查询条件
+     * @return com.xuecheng.base.model.PageResult<com.xuecheng.media.model.po.MediaFiles>
+     * @description 媒资文件查询方法
+     * @author Mr.M
+     * @date 2022/9/10 8:57
+     */
+    PageResult<MediaFiles> queryMediaFiels(Long companyId, PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto);
+
+}
