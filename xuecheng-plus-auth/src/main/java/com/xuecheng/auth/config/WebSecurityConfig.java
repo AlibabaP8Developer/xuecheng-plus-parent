677d51de048071c1f54cd979859cd5e4af7127b6
Edwin <1135365441@qq.com>
2023-06-10 23:08:38 +0800

    连接数据库查询用户信息进行认证

--- xuecheng-plus-auth/src/main/java/com/xuecheng/auth/config/WebSecurityConfig.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/config/WebSecurityConfig.java
@@ -1,9 +1,7 @@
 package com.xuecheng.auth.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,20 +24,20 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     //配置用户信息服务
-    @Bean
-    public UserDetailsService userDetailsService() {
-        //这里配置用户信息,这里暂时使用这种方式将用户存储在内存中
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("zhangsan").password("123").authorities("p1").build());
-        manager.createUser(User.withUsername("lisi").password("456").authorities("p2").build());
-        return manager;
-    }
+    //@Bean
+    //public UserDetailsService userDetailsService() {
+    //    //这里配置用户信息,这里暂时使用这种方式将用户存储在内存中
+    //    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    //    manager.createUser(User.withUsername("zhangsan").password("123").authorities("p1").build());
+    //    manager.createUser(User.withUsername("lisi").password("456").authorities("p2").build());
+    //    return manager;
+    //}
 
     @Bean
     public PasswordEncoder passwordEncoder() {
 //        //密码为明文方式
-        return NoOpPasswordEncoder.getInstance();
-//        return new BCryptPasswordEncoder();
+//        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     //配置安全拦截机制
@@ -58,4 +56,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+    public static void main(String[] args) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        // 生成密码
+        for (int i = 0; i < 5; i++) {
+            String encode = passwordEncoder.encode("111111");
+            System.out.println(encode);
+            // 校验密码
+            boolean matches = passwordEncoder.matches("111111", encode);
+            System.out.println(matches);
+        }
+    }
 }
---
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.xuecheng.ucenter.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.xuecheng.ucenter.mapper.XcUserMapper;
+import com.xuecheng.ucenter.model.po.XcUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private XcUserMapper userMapper;
+
+    /**
+     * 连接数据库查询用户信息
+     *
+     * @param username
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 根据username账号查询数据库
+        XcUser xcUser = this.userMapper.selectOne(new LambdaQueryWrapper<XcUser>().eq(XcUser::getUsername, username));
+        // 查询到用户不存在，要返回null即可，spring security框架抛出异常用户不存在
+        if (xcUser == null) {
+            return null;
+        }
+        // 如果查询到了用户，拿到正确的密码，最终封装成一个UserDetails对象给spring security框架返回，由框架进行密码比对
+        String password = xcUser.getPassword();
+        // 权限
+        String[] authorities = {"test"};
+        UserDetails userDetails = User.withUsername(username).password(password).authorities(authorities).build();
+        return userDetails;
+    }
+}
