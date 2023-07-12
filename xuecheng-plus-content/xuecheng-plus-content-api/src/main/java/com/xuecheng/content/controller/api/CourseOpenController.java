6dd5dc00351bb9b18271dcf9bda4e3a2d3e5ca42
Edwin <1135365441@qq.com>
2023-07-12 19:52:55 +0800

    根据课程ID查询课程信息

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/controller/api/CourseOpenController.java
@@ -0,0 +1,30 @@
+package com.xuecheng.content.controller.api;
+
+import com.xuecheng.content.model.dto.CoursePreviewDto;
+import com.xuecheng.content.service.CourseBaseInfoService;
+import com.xuecheng.content.service.CoursePublishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/open")
+public class CourseOpenController {
+
+    @Autowired
+    private CourseBaseInfoService courseBaseInfoService;
+
+    @Autowired
+    private CoursePublishService coursePublishService;
+
+    // 根据课程ID查询课程信息
+    @GetMapping("/course/whole/{courseId}")
+    public CoursePreviewDto getPreviewInfo(@PathVariable("courseId") Long courseId) {
+        //获取课程预览信息
+        CoursePreviewDto coursePreviewInfo = coursePublishService.getCoursePreviewInfo(courseId);
+        return coursePreviewInfo;
+    }
+
+}
