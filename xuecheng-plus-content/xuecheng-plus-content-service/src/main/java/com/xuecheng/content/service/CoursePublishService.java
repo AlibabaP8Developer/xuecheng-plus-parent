cdc3700088d93369a548abcc65780900a15f4454
Edwin <1135365441@qq.com>
2023-07-13 15:06:59 +0800

    课程提交审核

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CoursePublishService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CoursePublishService.java
@@ -13,4 +13,13 @@ public interface CoursePublishService {
      * @return
      */
     public CoursePreviewDto getCoursePreviewInfo(Long courseId);
+
+    /**
+     * @description 提交审核
+     * @param courseId  课程id
+     * @return void
+     * @author Mr.M
+     * @date 2022/9/18 10:31
+     */
+    public void commitAudit(Long companyId,Long courseId);
 }
--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CoursePublishServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CoursePublishServiceImpl.java
@@ -1,15 +1,26 @@
 package com.xuecheng.content.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.xuecheng.base.exception.XueChengPlusException;
+import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.mapper.CourseMarketMapper;
+import com.xuecheng.content.mapper.CoursePublishPreMapper;
 import com.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.xuecheng.content.model.dto.CoursePreviewDto;
 import com.xuecheng.content.model.dto.TeachplanDto;
+import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.model.po.CourseMarket;
+import com.xuecheng.content.model.po.CoursePublishPre;
 import com.xuecheng.content.service.CourseBaseInfoService;
 import com.xuecheng.content.service.CoursePublishService;
 import com.xuecheng.content.service.TeachplanService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -24,6 +35,15 @@ public class CoursePublishServiceImpl implements CoursePublishService {
     @Autowired
     private TeachplanService teachplanService;
 
+    @Autowired
+    private CourseMarketMapper courseMarketMapper;
+
+    @Autowired
+    private CoursePublishPreMapper coursePublishPreMapper;
+
+    @Autowired
+    private CourseBaseMapper courseBaseMapper;
+
     @Override
     public CoursePreviewDto getCoursePreviewInfo(Long courseId) {
         CoursePreviewDto coursePreviewDto = new CoursePreviewDto();
@@ -34,5 +54,70 @@ public class CoursePublishServiceImpl implements CoursePublishService {
         List<TeachplanDto> teachplayTree = teachplanService.findTeachplayTree(courseId);
         coursePreviewDto.setTeachplans(teachplayTree);
         return coursePreviewDto;
+    }
+
+    /**
+     * 提交审核
+     *
+     * @param companyId
+     * @param courseId  课程id
+     */
+    @Override
+    public void commitAudit(Long companyId, Long courseId) {
+        CourseBaseInfoDto courseBaseInfo = courseBaseInfoService.getCourseBaseInfo(courseId);
+        if (courseBaseInfo == null) {
+            XueChengPlusException.cast("课程找不到");
+        }
+        // 课程状态
+        String auditStatus = courseBaseInfo.getAuditStatus();
+
+        // 如果课程的审核状态为已提交则不允许提交
+        if (auditStatus.equals("202003")) {
+            XueChengPlusException.cast("课程已提交请等待审核");
+        }
+
+        // TODO 本机构只能提交本机构的课程
+
+        // 课程的图片、计划信息没有填写也不允许提交
+        String pic = courseBaseInfo.getPic();
+        if (StringUtils.isBlank(pic)) {
+            XueChengPlusException.cast("请求上传课程图片");
+        }
+        // 查询课程计划
+        List<TeachplanDto> teachplayTree = teachplanService.findTeachplayTree(courseId);
+        if (teachplayTree == null || teachplayTree.size() == 0) {
+            XueChengPlusException.cast("请填写课程计划");
+        }
+
+        // 查询到课程基本信息、营销信息、计划等信息插入到课程预发布表
+        CoursePublishPre coursePublishPre = new CoursePublishPre();
+        BeanUtils.copyProperties(courseBaseInfo, coursePublishPre);
+        // 设置机构ID
+        coursePublishPre.setCompanyId(companyId);
+        // 营销信息
+        CourseMarket courseMarket = courseMarketMapper.selectById(courseId);
+        String courseMarketJson = JSON.toJSONString(courseMarket);
+        coursePublishPre.setMarket(courseMarketJson);
+        // 计划信息
+        String teachplayTreeJson = JSON.toJSONString(teachplayTree);
+        coursePublishPre.setTeachplan(teachplayTreeJson);
+        // 状态为已提交
+        coursePublishPre.setStatus("202003");
+        // 提交时间
+        coursePublishPre.setCreateDate(LocalDateTime.now());
+        // 查询预发布表，如果有记录则更新，没有则插入
+        CoursePublishPre coursePublishPreObj = coursePublishPreMapper.selectById(courseId);
+        if (coursePublishPreObj == null) {
+            // 插入
+            coursePublishPreMapper.insert(coursePublishPre);
+        } else {
+            // 更新
+            coursePublishPreMapper.updateById(coursePublishPre);
+        }
+        // 更新课程基本信息表的审核状态为已提交
+        CourseBase courseBase = courseBaseMapper.selectById(courseId);
+        // 审核状态已提交
+        courseBase.setAuditStatus("202003");
+        courseBaseMapper.updateById(courseBase);
     }
 }
