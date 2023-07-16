c5eeb3e422a0317d9c6d101dcda77409e4245604
Edwin <1135365441@qq.com>
2023-07-16 22:12:09 +0800

    选课中心服务

---
+++ xuecheng-plus-learning/xuecheng-plus-learning-api/src/main/java/com/xuecheng/learning/api/MyCourseTablesController.java
@@ -0,0 +1,53 @@
+package com.xuecheng.learning.api;
+
+import com.xuecheng.base.exception.XueChengPlusException;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.learning.model.dto.MyCourseTableParams;
+import com.xuecheng.learning.model.dto.XcChooseCourseDto;
+import com.xuecheng.learning.model.dto.XcCourseTablesDto;
+import com.xuecheng.learning.model.po.XcCourseTables;
+import com.xuecheng.learning.util.SecurityUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 我的课程表接口
+ * @date 2022/10/25 9:40
+ */
+
+@Api(value = "我的课程表接口", tags = "我的课程表接口")
+@Slf4j
+@RestController
+public class MyCourseTablesController {
+
+
+    @ApiOperation("添加选课")
+    @PostMapping("/choosecourse/{courseId}")
+    public XcChooseCourseDto addChooseCourse(@PathVariable("courseId") Long courseId) {
+
+        return null;
+    }
+
+    @ApiOperation("查询学习资格")
+    @PostMapping("/choosecourse/learnstatus/{courseId}")
+    public XcCourseTablesDto getLearnstatus(@PathVariable("courseId") Long courseId) {
+
+        return null;
+
+    }
+
+    @ApiOperation("我的课程表")
+    @GetMapping("/mycoursetable")
+    public PageResult<XcCourseTables> mycoursetable(MyCourseTableParams params) {
+        return null;
+    }
+
+}
---
+++ xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/feignclient/ContentServiceClientFallbackFactory.java
@@ -0,0 +1,26 @@
+package com.xuecheng.learning.feignclient;
+
+import com.xuecheng.content.model.po.CoursePublish;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description TODO
+ * @date 2022/10/25 9:14
+ */
+@Slf4j
+@Component
+public class ContentServiceClientFallbackFactory implements FallbackFactory<ContentServiceClient> {
+    public ContentServiceClient create(final Throwable throwable) {
+        return new ContentServiceClient() {
+
+            public CoursePublish getCoursepublish(Long courseId) {
+                log.error("调用内容管理服务发生熔断:{}", throwable.toString(),throwable);
+                return null;
+            }
+        };
+    }
+}
